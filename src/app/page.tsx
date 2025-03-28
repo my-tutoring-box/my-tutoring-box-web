c16aebf2474d7086732b2d1db4c2d2b134c4d4bb
JIGOOOD <helmrping@gmail.com>
2025-03-28 22:39:31 +0900

    style: change padding

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <div className="container mx-auto py-50 px-5 sm:px-6 md:px-8 lg:px-10">
+    <div className="container mx-auto py-60 px-5 sm:px-6 md:px-8 lg:px-10">
       <h2 className="text-center text-5xl scroll-m-20 mt-50 pb-2 text-3xl font-semibold tracking-tight first:mt-0">
         마과박에 온 걸 환영합니다!
       </h2>

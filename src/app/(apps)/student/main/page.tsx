52a873d1c6d3efb0c27e95aaa0091891a32405a6
JIGOOOD <helmrping@gmail.com>
2025-04-13 04:01:21 +0900

    feat(student): add session information

--- src/app/(apps)/student/main/page.tsx
+++ src/app/(apps)/student/main/page.tsx
@@ -4,11 +4,13 @@ import { getCalenders, getLesson } from "@/app/lib/data/lesson.action";
 import { Calendar, Lesson } from "@/app/types/lesson.type";
 import DateBadgeList from "@/components/main/date-badge-list";
 import LessonInfo from "@/components/main/lesson-info";
-import { isAfter } from "date-fns";
+import { format, isAfter } from "date-fns";
+import { ko } from "date-fns/locale";
 import { useEffect, useState } from "react";
 
 export default function Page() {
   const [nextClass, setNextClass] = useState<Calendar | null>(null);
+  const [nextClassSession, setNextClassSession] = useState<number | null>(null);
   const [allDates, setAllDates] = useState<Calendar[]>([]);
   const [lesson, setLesson] = useState<Lesson | null>(null);
 
@@ -21,10 +23,11 @@ export default function Page() {
       setAllDates(data);
 
       const now = new Date();
-      const futureDates = data.filter((item) => isAfter(item.date, now));
+      const futureIndex = data.findIndex((item) => isAfter(item.date, now));
 
-      if (futureDates.length > 0) {
-        setNextClass(futureDates[0]);
+      if (futureIndex !== -1) {
+        setNextClass(data[futureIndex]);
+        setNextClassSession(futureIndex + 1);
       }
 
       // set lessons
@@ -38,6 +41,16 @@ export default function Page() {
   return (
     <div className="flex flex-col items-center justify-start p-6 md:p-10">
       <DateBadgeList allDates={allDates} nextClass={nextClass} />
+      <div className="flex fle-row gap-5 text-center text-2xl font-bold mt-4">
+        <p className="mb-2">
+          &lt;{nextClassSession}/{allDates.length} 일 째!&gt;
+        </p>
+        <p>
+          {nextClass?.date
+            ? format(new Date(nextClass.date), "M월 d일 EEE", { locale: ko })
+            : null}
+        </p>
+      </div>
       {lesson && (
         <div className="w-full max-w-lg mt-8">
           <LessonInfo lesson={lesson} />

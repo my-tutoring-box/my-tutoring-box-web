8e1f42d11f9503db703ab05f2c063c2934521148
JIGOOOD <helmrping@gmail.com>
2025-08-17 20:28:09 +0900

    feat: add docker file

---
+++ Dockerfile
@@ -0,0 +1,18 @@
+# 빌드 단계
+FROM node:22-alpine AS build
+WORKDIR /app
+COPY package*.json .
+RUN npm install
+COPY . .
+RUN npm run build
+
+# 실행 단계
+FROM node:22-alpine AS production
+WORKDIR /app
+COPY --from=build /app/.next ./.next
+COPY --from=build /app/public ./public
+COPY --from=build /app/package*.json ./
+RUN npm install --omit=dev
+
+EXPOSE 3001
+CMD ["npm", "start"]
--- src/app/(apps)/teacher/main/page.tsx
+++ src/app/(apps)/teacher/main/page.tsx
@@ -1,55 +1,10 @@
-"use client";
+import { Suspense } from "react";
+import MainPage from "./main";
 
-import { useEffect, useState } from "react";
-import { getStudents, setStudentId } from "@/app/lib/data/student.action";
-import { Student } from "@/app/types/student.type";
-import { useRouter, useSearchParams } from "next/navigation";
-import StudentList from "@/components/main/student-list";
-import StudentForm from "@/components/main/student-form";
-import { Button } from "@/components/ui/button";
-import { Plus } from "lucide-react";
-
-export default function MainPage() {
-  const [students, setStudents] = useState<Student[]>([]);
-  const searchParams = useSearchParams();
-  const router = useRouter();
-
-  const showForm = searchParams.get("add") === "1";
-  const refresh = searchParams.get("refresh") === "1";
-
-  useEffect(() => {
-    const fetch = async () => {
-      const data = await getStudents();
-      setStudents(data);
-    };
-    fetch();
-  }, [refresh]);
-
-  useEffect(() => {
-    if (refresh) {
-      router.replace("/teacher/main");
-    }
-  }, [refresh]);
-
+export default function Page() {
   return (
-    <div className="px-6 py-8">
-      <div className="flex justify-end mb-4">
-        <Button onClick={() => router.push("?add=1")}>
-          <Plus />
-        </Button>
-      </div>
-
-      {showForm ? (
-        <StudentForm />
-      ) : (
-        <StudentList
-          students={students}
-          onCardClick={async (student) => {
-            await setStudentId(student._id);
-            router.push(`/teacher/main/${student._id}`);
-          }}
-        />
-      )}
-    </div>
+    <Suspense>
+      <MainPage />
+    </Suspense>
   );
 }
